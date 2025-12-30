e03d3381b5db9ea39451354a62a9a69c1a07f777
Erin Kerr <erin.kerr17@gmail.com>
2025-12-30 14:29:25 -0800

    Update app configuration and dependencies for improved performance and compatibility
    - Refined app.json to include a new scheme and updated iOS build settings, enhancing overall configuration.
    - Adjusted package.json to downgrade certain dependencies for better compatibility, including @expo/vector-icons and react-native-async-storage.
    - Updated babel.config.js to improve JSX handling and added react-native-reanimated plugin for enhanced animation support.
    - Modified package-lock.json to reflect changes in dependency versions, ensuring a more stable build environment.
    
    These changes enhance the app's configuration and dependency management, improving performance and compatibility across platforms.

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,11 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: [
-      'babel-preset-expo',
-      ['nativewind/babel'],
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel',
     ],
     plugins: [
+      'react-native-reanimated/plugin',
       [
         'module-resolver',
         {
